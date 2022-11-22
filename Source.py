5fe99154f23ba469ea9980a7de698a7427677619
scott <stanch@uvm.edu>
2022-11-22 15:42:39 -0500

    updates to the source and tools from the pi

--- Source.py
+++ Source.py
@@ -2,60 +2,66 @@ import socket
 import pickle
 import transfer_tools as tls
 import time
-
-# outline
-# get a list of all DZT in the given directory
-# Send the server a directory name
-    # Server checks for the existance and contents of the provided directory
-    # Server retiurns a list of the contained DZTs
-# Do disjunctio of the two lists i.e. lists on the source not on the server, save as 
-# 
+import cv2
 
 # Constants
-search_path = "C:\\Users\\Scott\\Desktop\\GPR_DATA\\Lincoln_Hill\\" 
+search_path = "/home/stanch/public/DZTs"
 #Host = '65.183.134.63'
-Host = '127.0.0.1'
+Host = '192.168.1.206'
 Port = 55
+delay = 30
 
-source_stack = tls.full_stack(search_path)
 
-next_up = source_stack.pop()
-dzt_obj = tls.DZT_DAT(next_up)
-pickled_dzt = pickle.dumps(dzt_obj)
-server_stack = ['empty']
 print("Waiting for Connection to Host")
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.connect((Host, Port))
     print("Connected to ", Host)
     
+    # Are we doing F scans or B scans?
+    choice = False
+    while choice == False:
+        scan_mode = input("Select Scan Mode: [F/B] ")
+        if scan_mode == 'F' or 'f':
+            f_mode = True
+            choice = True
+        elif scan_mode == 'B' or 'b':
+            f_mode = False
+            choice = True
+        else:
+            print("Selection not understood")
+    
     # Prompt the user for the name of the survery and send it off
     print("Sending Directory...")
-    #directory =  "DIR " + input("Enter Survey Directory: ")
-    directory = "DIR Lincoln_Hill"
+    directory =  "DIR " + input("Enter Survey Directory: ")
     tls.gen_send(s,directory)
-    response = tls.gen_recv(s)
-    print("response: ",response)
-    
-    # Send the list of files on the source machine, need to recive the list of files on the server
-    print("Sending Source Stack...")
-    tls.gen_send(s,source_stack)
-    response = tls.gen_recv(s)
-    print("response: ",response)
-    request_stack = tls.gen_recv(s)
-    print("request stack: ",request_stack)
-    
-    print("Sending Files...")
-    while request_stack != []:
-        next_file = request_stack.pop()
-        print("Sending ",next_file)
-        next_dzt = tls.DZT_DAT(next_file)
-        tls.gen_send(s,next_dzt)
-        response = tls.gen_recv(s)
-        print(response)
-    print("Stack Empty")
+    ACK_response = tls.gen_recv(s)
+
+    while True:
+        # Send the list of files on the source machine, need to recive the list of files on the server
+        print("Sending Source Stack...")
+        source_stack = tls.full_stack(search_path)
+        tls.gen_send(s,source_stack)
+        request_stack = tls.gen_recv(s)
+        if request_stack != []:
+            print("Sending Files...")
+            while request_stack != []:
+                next_file = request_stack.pop()
+                print("Sending ",next_file)
+                next_dzt = tls.DZT_DAT(next_file,f_mode)
+                print(next_dzt.file_name)
+                print(next_dzt.realsense_file)
+                tls.gen_send(s,next_dzt)
+                b_scan = tls.gen_recv(s)
+                print("/home/stanch/public/b_scans/"+next_file.split('.')[0]+".png")
+                cv2.imwrite("/home/stanch/public/b_scans/"+next_file.split('.')[0]+".png",b_scan)
+                #ACK_response = tls.gen_recv(s)
+            print("Stack Empty")
+        else:
+            print("No requests, waiting for more files....")
+            time.sleep(delay)
     
     # Close the socket
     print("Closing Connection...")
     tls.gen_send(s,"COM exit")
     response = tls.gen_recv(s)
-    print("response: ",response)
+    #print("response: ",response)
